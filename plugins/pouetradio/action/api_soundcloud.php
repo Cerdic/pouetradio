62e2c7de5c4f196697b434537d0dc80d57af191c
Cerdic <cedric@yterium.com>
2018-08-02 14:48:00 +0200

    API Soundcloud

---
+++ plugins/pouetradio/action/api_soundcloud.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Plugin PouetRadio
+ * (c) 2017
+ *
+ */
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function action_api_soundcloud_dist() {
+
+	$arg = _request('arg');
+	$arg = explode('/', $arg);
+
+	$soundcloud_url = base64_decode(end($arg));
+	if (!function_exists('calculer_cle_action')) {
+		include_spip("inc/securiser_action");
+	}
+
+	$cle = calculer_cle_action("soundcloud:" . $soundcloud_url);
+	if ($cle === reset($arg) and defined('_SOUNDCLOUD_CLIENT_ID')) {
+		$api_url = "http://api.soundcloud.com/resolve?client_id="._SOUNDCLOUD_CLIENT_ID;
+		$api_url = parametre_url($api_url, "url", $soundcloud_url);
+		include_spip('inc/distant');
+		$res = recuperer_url($api_url, array('methode' => 'HEAD', 'follow_location' => 0));
+		if ($res and $res['location']) {
+			$tracks_url = $res['location'];
+			$stream_url = str_replace("?", "/stream?", $tracks_url);
+			$res = recuperer_url($stream_url, array('methode' => 'HEAD', 'follow_location' => 0));
+			$media_url = $res['location'];
+			if ($res and $res['location']) {
+				include_spip('inc/headers');
+				redirige_par_entete($media_url);
+			}
+		}
+	}
+
+
+}
